910a64ecd03926a76643c8161226d81f35e32706
Vitor Trimer <vitortrimer@gmail.com>
2021-01-20 09:52:24 -0300

    Add login page UI

---
+++ InstagramFirestore/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestore
+//
+//  Created by Vitor Trimer on 20/01/21.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    
+}
