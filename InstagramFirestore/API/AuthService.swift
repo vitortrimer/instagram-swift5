cd3a45e4731ec51867354e71e809a97e2f5f3f3e
Vitor Trimer <vitortrimer@gmail.com>
2021-01-20 22:12:46 -0300

    Connect registration with the API

--- InstagramFirestore/API/AuthService.swift
+++ InstagramFirestore/API/AuthService.swift
@@ -17,7 +17,25 @@ struct AuthCredentials {
 }
 
 struct AuthService {
-    static func registerUser(withCredentials credentials: AuthCredentials) {
-        print("DEBUG: Credentials are \(credentials)")
+    static func registerUser(withCredentials credentials: AuthCredentials, completion: @escaping(Error?) -> Void) {
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+                
+                if let error = error {
+                    print("DEBUG: Failed to register user \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "username": credentials.username,
+                                           "password": credentials.password,
+                                           "uid": uid,
+                                           "profileImageUrl": imageUrl]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
     }
 }
--- InstagramFirestore/Controller/Authentication/RegistrationController.swift
+++ InstagramFirestore/Controller/Authentication/RegistrationController.swift
@@ -106,12 +106,19 @@ class RegistrationController: UIViewController {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         guard let fullname = fullNameTextField.text else { return }
-        guard let username = usernameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
         guard let profileImage = self.profileImage else { return }
         
         let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
         
-        AuthService.registerUser(withCredentials: credentials)
+        AuthService.registerUser(withCredentials: credentials) { error in
+            if let error = error {
+                print("DEBUG: Failed to register user \(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: User registrated successfully")
+        }
     }
     
     //MARK: - Helpers
