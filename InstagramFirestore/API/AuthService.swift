4007efde8d0413e9240345103cc19366d7264824
Vitor Trimer <vitortrimer@gmail.com>
2021-01-20 21:47:51 -0300

    Update git ignore

---
+++ InstagramFirestore/API/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  InstagramFirestore
+//
+//  Created by Vitor Trimer on 20/01/21.
+//
+
+import Foundation
---
+++ InstagramFirestore/API/ImageUploader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageUploader.swift
+//  InstagramFirestore
+//
+//  Created by Vitor Trimer on 20/01/21.
+//
+
+import Foundation
